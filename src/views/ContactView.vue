8e981db6dfe7efe332c08cbdf4d4ccf7a9df66e3
chloe <chloealmazien6@gmail.com>
2025-03-26 11:22:48 +0200

    added an outline

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -27,20 +27,20 @@
                 <form action="https://formspree.io/f/xnqewyrd" method="post" role="form" class="php-email-form">
                   <div class="row mt-5">
                     <div class="col-md-6 form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required  style="border: 2px solid black;">
                     </div>
                     <div class="col-md-6 form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Surname" required>
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Surname" required  style="border: 2px solid black;">
                     </div>
                     <div class="form-group mt-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required  style="border: 2px solid black;">
                     </div>
                   </div>
                   <div class="form-group mt-3">
-                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required  style="border: 2px solid black;">
                   </div>
                   <div class="form-group mt-3">
-                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                    <textarea class="form-control" name="message" rows="5" placeholder="Message" required  style="border: 2px solid black;"></textarea>
                   </div>
                   <div class="text-center mt-4"><button type="submit"><i>SUBMIT</i></button></div>
                 </form>
@@ -66,5 +66,6 @@ export default {
  h1{
   font-style: italic;
  }
+ 
 </style>
       
